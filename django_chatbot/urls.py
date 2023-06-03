2b96bb303d3d4e90cdfee294215ba68c8f86f40a
Essam Anwar <102623413+Essam-2@users.noreply.github.com>
2023-06-03 14:53:41 +0300

    [+] import get csrf token function

--- django_chatbot/urls.py
+++ django_chatbot/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .views import index, get_csrf_token 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
