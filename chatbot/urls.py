f5f2836a3b2539da0d6710231b92828403219bb4
Essam Anwar <102623413+Essam-2@users.noreply.github.com>
2023-06-03 14:51:48 +0300

    [+] adding path for csrf token

--- chatbot/urls.py
+++ chatbot/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.chatbot, name='chatbot')
+    path('', views.chatbot, name='chatbot'),
+    path('api/csrf_token/', views.get_csrf_token, name='get_csrf_token')
 ]
